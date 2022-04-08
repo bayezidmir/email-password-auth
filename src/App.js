31edbe2debf8b1d81b5a5b6496fc1bba0360ae5a
bayezid mir <warner.roman@gmail.com>
2022-04-08 07:42:40 +0600

    applying regular expression

--- src/App.js
+++ src/App.js
@@ -1,54 +1,82 @@
 import logo from "./logo.svg";
 import "./App.css";
 import app from "./firebase.init";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 const auth = getAuth(app);
 
 function App() {
-  const [user, setUser] = useState({});
-  const provider = new GoogleAuthProvider();
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  // Google sign in
-  const googleLoginHandler = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        setUser(result.user);
-        console.log(result.user);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  const handleEmailInput = (e) => {
+    setEmail(e.target.value);
   };
 
-  // google sign out
-  const googleHandleLogOut = () => {
-    const auth = getAuth();
-    signOut(auth)
-      .then(() => {
-        setUser({});
-      })
-      .catch((error) => {
-        setUser({});
-      });
+  const handleEmailPassChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => console.log(result.user))
+      .catch((error) => console.log(error));
+    e.preventDefault();
   };
 
   return (
     <div className="App">
-      <h1>Simple Firebase Auth</h1>
-      <img src={user.photoURL} alt="" />
-      <p>Name: {user.displayName}</p>
-      {user.uid ? (
-        <button onClick={googleHandleLogOut}>Google Log Out</button>
-      ) : (
-        <button onClick={googleLoginHandler}>Google Log In</button>
-      )}
+      <h1>Create User With Email and Pass</h1>
+
+      <Form
+        className="w-50 mx-auto"
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+      >
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            required
+            type="email"
+            placeholder="Enter email"
+            onBlur={handleEmailInput}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            required
+            type="password"
+            placeholder="Password"
+            onBlur={handleEmailPassChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
